ad09db9b2bf68621212192f7a518008c0328dc88
Fikir Aliya <fikirtesfaye2@gmail.com>
2022-12-22 06:23:14 +0000

    Full deployment

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+'''fcreates and distributes an archive to your web servers, using deploy():
+'''
+
+import os
+from datetime import datetime
+from fabric.api import env, local, put, run, runs_once
+
+
+env.hosts = ['100.26.234.212', '54.90.48.243']
+
+
+@runs_once
+def do_pack():
+    """Archives the static files."""
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    cur_time = datetime.now()
+    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        cur_time.year,
+        cur_time.month,
+        cur_time.day,
+        cur_time.hour,
+        cur_time.minute,
+        cur_time.second
+    )
+    try:
+        print("Packing web_static to {}".format(output))
+        local("tar -cvzf {} web_static".format(output))
+        archize_size = os.stat(output).st_size
+        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
+    except Exception:
+        output = None
+    return output
